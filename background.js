ad46ffe3e3a95b13e279212dafdc1a890e626df3
pizdoc <denis.nyukhalov@gmail.com>
2024-04-10 14:33:49 +0400

    notify & webm -> mp4

--- background.js
+++ background.js
@@ -6,50 +6,43 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
+function notify(title, message) {
+    chrome.notifications.create({
+        type: "basic",
+        iconUrl: "icon.png",
+        title: title,
+        message: message
+    })
+}
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "sendVideo") {
         chrome.storage.sync.get(["token", "chat_id"], function (data) {
+            console.log(data)
             if (!data.token || !data.chat_id) {
                 throw new Error("Token or chat_id not found");
             }
 
-            fetch("https://api.telegram.org/bot${data.token}/sendVideo", {
+            fetch(`https://api.telegram.org/bot${data.token}/sendVideo`, {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
                     chat_id: data.chat_id,
-                    video: info.srcUrl
+                    video: info.srcUrl.replace(".webm", ".mp4")
                 })
             })
                 .then(response => {
                     if (!response.ok) {
-                        chrome.notifications.create({
-                            type: "basic",
-                            iconUrl: "icon.png",
-                            title: "Error",
-                            message: "Network response was not ok",
-                        })
+                        notify("Error", "Network response was not ok")
                         throw new Error("Network response was not ok");
                     }
                     return response.json();
                 })
                 .then(data => {
-                    chrome.notifications.create({
-                        type: "basic",
-                        iconUrl: "icon.png",
-                        title: "Success",
-                        message: "Video sent successfully to chat",
-                    })
+                    notify("Success", "Video sent successfully to chat")
                 })
                 .catch((error) => {
-                    chrome.notifications.create({
-                        type: "basic",
-                        iconUrl: "icon.png",
-                        title: "Error",
-                        message: error.message,
-                    })
+                    notify("Error", error.message)
                 });
         });
     }
